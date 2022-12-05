938b62a41587c88dc27b58409f7f67836755d1c6
Edilson Torres <edilsontor77@gmail.com>
2022-12-05 11:59:38 -0300

    Alterando migration

--- database/migrations/1668071567287_companies.ts
+++ database/migrations/1668071567287_companies.ts
@@ -5,16 +5,16 @@ export default class extends BaseSchema {
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-      table.string('name').notNullable()
+      table.increments('id');
+      table.string('name').notNullable();
       table.dateTime('initial_date').nullable();
-      table.dateTime('final_date').nullable()
-      table.string('description', 100).nullable()
-      table.boolean('is_done').defaultTo(false)
-      table.integer('user_id').unsigned().references('id').inTable('users')
+      table.dateTime('final_date').nullable();
+      table.string('description', 100).nullable();
+      table.boolean('is_done').defaultTo(false);
+      table.integer('user_id').unsigned().references('id').inTable('users');
 
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+      table.timestamp('created_at', { useTz: true });
+      table.timestamp('updated_at', { useTz: true });
     })
   }
 
